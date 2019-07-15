38d782b68468fb8341dbd9769110231b328abb1f
Denis Sidorov <dsigmat@hotmail.com>
2019-07-15 16:10:20 +0600

    Добавление представления

--- MVCMovie/Controllers/HelloWorldController.cs
+++ MVCMovie/Controllers/HelloWorldController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,19 +9,33 @@ namespace MVCMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
         // 
         // GET: /HelloWorld/
-        public string Index()
-        {
-            return "This is my default action...";
-        }
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
 
 
         // 
         // GET: /HelloWorld/Welcome/ 
-        public string Welcome()
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+
+        public string Welcome(string name, int ID = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
