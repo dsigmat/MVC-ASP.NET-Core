274d18d8b22661c1d4fa6bc728dd10d9c4d91ae6
Denis Sidorov <dsigmat@hotmail.com>
2019-07-18 16:18:06 +0600

    Добавление нового поля в приложение MVC ASP.NET Core Добавление свойства Rating в модель Movie

--- MVCMovie/Migrations/20190718101620_Rating.Designer.cs
+++ MVCMovie/Migrations/20190718101620_Rating.Designer.cs
@@ -4,14 +4,16 @@ using MVCMovie.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVCMovie.Migrations
 {
     [DbContext(typeof(MVCMovieContext))]
-    partial class MVCMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20190718101620_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
